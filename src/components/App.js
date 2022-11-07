98ba9200b6a63d802d418ff454b6b2860136d04e
Egor Talovikov <egtalovikov@gmail.com>
2022-11-07 17:02:34 +0300

    feat: реализовал декларативный подход при открытии и закрытии попапов

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,103 @@
+import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer'
+import PopupWithForm from './PopupWithForm';
 
 function App() {
+  const [isEditProfilePopupOpen, setEditProfile] = React.useState(false);
+  const [isAddPlacePopupOpen, setAddPlace] = React.useState(false);
+  const [isEditAvatarPopupOpen, setEditAvatar] = React.useState(false);
+
+  function handleEditAvatarClick() {
+    setEditAvatar(true);
+  }
+
+  function handleEditProfileClick() {
+    setEditProfile(true);
+  }
+
+  function handleAddPlaceClick() {
+    setAddPlace(true);
+  }
+
+  function closeAllPopups(evt) {
+    if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close-button')) {
+      setEditAvatar(false);
+      setEditProfile(false);
+      setAddPlace(false);
+    }
+  }
+
   return (
     <>
       <div className="page">
         <div className="page__container">
-          <Header/>
-          <Main/>
-          <Footer/>
-
-
-        <div className="popup popup_image">
-          <div className="popup__image-container">
-            <button type="button" aria-label="Закрытие попапа" className="popup__close-button buttons"></button>
-            <figure className="popup__figure">
-              <img src="#" alt="" className="popup__image" />
-              <figcaption className="popup__image-caption"></figcaption>
-            </figure>
-          </div>
-        </div>
-
-        <template className="post-template">
-          <article className="post">
-            <img src="#" alt="" className="post__photo" />
-            <button type="button" aria-label="Удаление карточки" className="post__delete-button"></button>
-            <div className="post__bottom">
-              <h2 className="post__title">#</h2>
-              <div className="post__like-block">
-                <button type="button" aria-label="Лайк" className="post__like"></button>
-                <p className="post__like-counter"></p>
-              </div>
+          <Header />
+          <Main onEditAvatar={handleEditAvatarClick} onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} />
+          <Footer />
+          <PopupWithForm isOpen={isEditAvatarPopupOpen} name={'avatar'} title={'Обновить аватар'} onClose={closeAllPopups}>
+            <div class="popup__field">
+              <input type="url" placeholder="Ссылка на аватар" name="avatar" class="popup__input popup__input_type_avatar"
+                id="avatar-input" required />
+              <span class="popup__error avatar-input-error"></span>
             </div>
-          </article>
-        </template>
+            <button type="submit" class="popup__button buttons">
+              Сохранить
+            </button>
+          </PopupWithForm>
+          <PopupWithForm name={'confirmation'} title={'Вы уверены?'} onClose={closeAllPopups}>
+            <button type="button" class="popup__button buttons">
+              Да
+            </button>
+          </PopupWithForm>
+          <PopupWithForm isOpen={isAddPlacePopupOpen} name={'add'} title={'Новое место'} onClose={closeAllPopups}>
+            <div class="popup__field">
+              <input type="text" placeholder="Название" name="postname" class="popup__input popup__input_type_postname"
+                id="postname-input" minlength="2" maxlength="30" required />
+              <span class="popup__error postname-input-error"></span>
+            </div>
+            <div class="popup__field">
+              <input type="url" placeholder="Ссылка на картинку" name="link" class="popup__input popup__input_type_link"
+                id="link-input" required />
+              <span class="popup__error link-input-error"></span>
+            </div>
+            <button type="submit" class="popup__button buttons">
+              Создать
+            </button>
+          </PopupWithForm>
+          <PopupWithForm isOpen={isEditProfilePopupOpen} name={'edit'} title={'Редактировать профиль'} onClose={closeAllPopups}>
+            <div class="popup__field">
+              <input type="text" placeholder="Имя" name="name" class="popup__input popup__input_type_name" id="name-input"
+                required minlength="2" maxlength="40" />
+              <span class="popup__error name-input-error"></span>
+            </div>
+            <div class="popup__field">
+              <input type="text" placeholder="О Себе" name="about" class="popup__input popup__input_type_about" id="about-input"
+                required minlength="2" maxlength="200" />
+              <span class="popup__error about-input-error"></span>
+            </div>
+            <button type="submit" class="popup__button buttons">
+              Сохранить
+            </button>
+          </PopupWithForm>
+
+
+
+
+          <template className="post-template">
+            <article className="post">
+              <img src="#" alt="" className="post__photo" />
+              <button type="button" aria-label="Удаление карточки" className="post__delete-button"></button>
+              <div className="post__bottom">
+                <h2 className="post__title">#</h2>
+                <div className="post__like-block">
+                  <button type="button" aria-label="Лайк" className="post__like"></button>
+                  <p className="post__like-counter"></p>
+                </div>
+              </div>
+            </article>
+          </template>
         </div>
       </div>
     </>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,6 +1,6 @@
 function PopupWithForm(props) {
   return (
-      <div className={`popup popup_${props.name}`}>
+      <div onClick={(evt) => props.onClose(evt)} className={`popup popup_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}>
         <div className="popup__container">
           <button type="button" aria-label="Закрытие попапа" className="popup__close-button buttons"></button>
           <h2 className="popup__title">{props.title}</h2>
