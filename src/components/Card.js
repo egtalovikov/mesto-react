54b4c3a9d21c3e45afd4e745d77c28a9ae6876c9
Egor Talovikov <egtalovikov@gmail.com>
2022-11-21 02:50:34 +0300

    feat: использовал контекст в 'Card'

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,27 @@
+import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
 function Card(props) {
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const isOwn = props.card.owner._id === currentUser._id;
+
+  const cardDeleteButtonClassName = (
+    `post__delete-button ${isOwn ? 'post__delete-button_visible' : ''}`
+  );
+
+  const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+
+  const cardLikeButtonClassName = `...`;
+
   function handleClick() {
     props.onCardClick(props.card);
-  }  
+  }
 
   return (
     <article className="post">
       <img onClick={handleClick} src={props.card.link} alt={props.card.name} className="post__photo" />
-      <button type="button" aria-label="Удаление карточки" className="post__delete-button"></button>
+      <button type="button" aria-label="Удаление карточки" className={cardDeleteButtonClassName}></button>
       <div className="post__bottom">
         <h2 className="post__title">{props.card.name}</h2>
         <div className="post__like-block">
